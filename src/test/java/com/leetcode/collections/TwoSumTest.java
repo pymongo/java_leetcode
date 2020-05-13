ed63b628bea2851b69f142abbcc797329f1cd57c
吴翱翔 <pylint@yandex.com>
2020-05-13 17:40:46 +0800

    添加了第一个单元测试文件

---
+++ src/test/java/com/leetcode/collections/TwoSumTest.java
@@ -0,0 +1,11 @@
+package com.leetcode.collections;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TwoSumTest {
+    @Test
+    void twoSumTest() {
+        assertEquals(1, 2);
+    }
+}
