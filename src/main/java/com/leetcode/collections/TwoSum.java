9a5410a549d9f2c6239141caebe763d5138e9f36
吴翱翔 <pylint@yandex.com>
2020-05-13 17:51:10 +0800

    完成了leetcode twosum的单元测试

---
+++ src/main/java/com/leetcode/collections/TwoSum.java
@@ -0,0 +1,8 @@
+package com.leetcode.collections;
+
+public class TwoSum {
+    public static int[] twoSum(int[] nums, int target) {
+//        throw new IllegalArgumentException("No solution");
+        return new int[] {0, 1};
+    }
+}
--- src/test/java/com/leetcode/collections/TwoSumTest.java
+++ src/test/java/com/leetcode/collections/TwoSumTest.java
@@ -1,11 +1,13 @@
 package com.leetcode.collections;
 
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 public class TwoSumTest {
     @Test
     void twoSumTest() {
-        assertEquals(1, 2);
+        int[] result = TwoSum.twoSum(new int[]{2, 7, 11, 15}, 9);
+        assertArrayEquals(result, new int[]{0, 1});
     }
 }
