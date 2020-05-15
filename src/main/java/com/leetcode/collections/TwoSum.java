d56d34fb999fc80ed8ef43dc6b30f58e4f1e7b79
吴翱翔 <pylint@yandex.com>
2020-05-15 19:46:06 +0800

    删掉了多余的main文件夹

--- src/main/java/com/leetcode/collections/TwoSum.java
+++
@@ -1,8 +0,0 @@
-package com.leetcode.collections;
-
-public class TwoSum {
-    public static int[] twoSum(int[] nums, int target) {
-//        throw new IllegalArgumentException("No solution");
-        return new int[] {0, 1};
-    }
-}
