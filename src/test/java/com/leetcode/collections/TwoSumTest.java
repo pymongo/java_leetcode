93e50e12fe4b7d4a5bd2e0d2bb9835161489f9c1
吴翱翔 <pylint@yandex.com>
2020-05-15 20:36:00 +0800

    做完了TwoSum的HashMap解法

--- src/test/java/com/leetcode/collections/TwoSumTest.java
+++ src/test/java/com/leetcode/collections/TwoSumTest.java
@@ -1,13 +1,30 @@
 package com.leetcode.collections;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import java.util.HashMap;
+import java.util.Map;
 
 public class TwoSumTest {
+
+    // 21968ns 20565ns 19601ns
+    public static int[] twoSum(int[] nums, int target) {
+//        long start = System.nanoTime();
+        Map<Integer, Integer> sumTracker = new HashMap<Integer, Integer>();
+        for (int i = 0; i < nums.length; i++) {
+            if (sumTracker.containsKey(nums[i])) {
+//                System.out.println((System.nanoTime() - start) + "ns");
+                return new int[]{sumTracker.get(nums[i]), i};
+            }
+            sumTracker.put(target - nums[i], i);
+        }
+        throw new IllegalArgumentException("No solution");
+    }
+
     @Test
     void twoSumTest() {
-        int[] result = TwoSum.twoSum(new int[]{2, 7, 11, 15}, 9);
-        assertArrayEquals(result, new int[]{0, 1});
+        int[] result = twoSum(new int[]{2, 7, 11, 15}, 9);
+        Assertions.assertArrayEquals(result, new int[]{0, 1});
     }
 }
